eb699ddd1a36273abff87ff2d3377cf9beeaebf3
Pravin3102 <pravint@ideas2it.com>
2019-11-05 19:28:31 +0530

    Authentication

---
+++ src/services/users/users.mongoose.js
@@ -0,0 +1,37 @@
+
+/* eslint quotes: 0 */
+// Defines Mongoose model for service `users`. (Can be re-generated.)
+const merge = require('lodash.merge');
+// eslint-disable-next-line no-unused-vars
+const mongoose = require('mongoose');
+// !code: imports // !end
+// !code: init // !end
+
+let moduleExports = merge({},
+  // !<DEFAULT> code: model
+  {
+    name: {
+      type: String,
+      minLength: 3,
+      maxLength: 15
+    },
+    phone: {
+      type: Number,
+      match: "^[0-9]{10}$",
+      minLength: 10,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  // !end
+  // !code: moduleExports // !end
+);
+
+// !code: exports // !end
+module.exports = moduleExports;
+
+// !code: funcs // !end
+// !code: end // !end
